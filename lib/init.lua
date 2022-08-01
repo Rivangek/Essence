96f8fc79d032c92216fd89484883ae0fc7724026
Rivangek <ank303442@gmail.com>
2022-08-01 13:46:48 -0500

    Format Code

--- lib/init.lua
+++ lib/init.lua
@@ -8,120 +8,120 @@ local Element = require(Classes.Element)
 --
 
 function Essence.new(...)
-   local Parameters = {...}
+	local Parameters = {...}
 
-   local ClassObject: string | { any } = Parameters[1] -- It can be an string containing the ClassName
-   local Props = Parameters[2]
+	local ClassObject: string | { any } = Parameters[1] -- It can be an string containing the ClassName
+	local Props = Parameters[2]
 
-   if typeof(ClassObject) == "table" then -- It is a component.
-      local ElementObject = ClassObject:Render(Props)
+	if typeof(ClassObject) == "table" then -- It is a component.
+		local ElementObject = ClassObject:Render(Props)
 
-      for Index, Value in ClassObject do
-         if typeof(Value) == "function" and not (Index == "Render") then
-            ElementObject.Functions[Index] = Value
-         end
-      end
+		for Index, Value in ClassObject do
+			if typeof(Value) == "function" and not (Index == "Render") then
+				ElementObject.Functions[Index] = Value
+			end
+		end
 
-      return ElementObject;
-   end
+		return ElementObject;
+	end
 
-   -- Switch not passed, it's a Roblox class.
+	-- Switch not passed, it's a Roblox class.
 
-   local ElementProperties = Props
-   ElementProperties.ClassName = ClassObject
+	local ElementProperties = Props
+	ElementProperties.ClassName = ClassObject
 
-   local ElementObject = Element.new(ElementProperties)
-   return ElementObject
+	local ElementObject = Element.new(ElementProperties)
+	return ElementObject
 end
 
 function Essence.build(ElementObject: Element.Class)
-   local ElementInstance = Instance.new(ElementObject.ClassName)
-   local OriginalProperties = ElementObject.OriginalProperties
+	local ElementInstance = Instance.new(ElementObject.ClassName)
+	local OriginalProperties = ElementObject.OriginalProperties
 
-   --
+	--
 
-   local ProductionElement = {}
-   ProductionElement.Children = {}
+	local ProductionElement = {}
+	ProductionElement.Children = {}
 
-   local ProductionState = {}
-   local ProductionChildren = {}
+	local ProductionState = {}
+	local ProductionChildren = {}
 
-   --
+	--
 
-   local ProductionElementMeta = {
-      __index = function(tabl: { any }, index: string)
-         if index == "Instance" then
-            return ElementInstance
-         elseif index == "State" then
-            return ProductionState
-         end
+	local ProductionElementMeta = {
+		__index = function(tabl: { any }, index: string)
+			if index == "Instance" then
+				return ElementInstance
+			elseif index == "State" then
+				return ProductionState
+			end
 
-         if ProductionChildren[index] then
-            return ProductionChildren[index]
-         end
-      end
-   }
+			if ProductionChildren[index] then
+				return ProductionChildren[index]
+			end
+		end
+	}
 
-   local ProductionStateMeta = {
-      __newindex = function(_, index: string, value: any)
-         for Property: string, Value: any in OriginalProperties do
-            if typeof(Value) == "table" and Value.IsEssenceState then
-               local StateIdentifier = Value.StateIdentifier
+	local ProductionStateMeta = {
+		__newindex = function(_, index: string, value: any)
+			for Property: string, Value: any in OriginalProperties do
+				if typeof(Value) == "table" and Value.IsEssenceState then
+					local StateIdentifier = Value.StateIdentifier
 
-               if index == StateIdentifier then
-                  if Value.StateCompute then
-                     ElementInstance[Property] = Value.StateCompute(value)
-                  else
-                     ElementInstance[Property] = value
-                  end
+					if index == StateIdentifier then
+						if Value.StateCompute then
+							ElementInstance[Property] = Value.StateCompute(value)
+						else
+							ElementInstance[Property] = value
+						end
 
-                  ElementObject:SetState(StateIdentifier, value)
-               end
-            end
-         end
-      end,
+						ElementObject:SetState(StateIdentifier, value)
+					end
+				end
+			end
+		end,
 
-      __index = function(_, index: string)
-         return ElementObject:GetState(index)
-      end
-   }
+		__index = function(_, index: string)
+			return ElementObject:GetState(index)
+		end
+	}
 
-   setmetatable(ProductionElement, ProductionElementMeta)
-   setmetatable(ProductionState, ProductionStateMeta)
+	setmetatable(ProductionElement, ProductionElementMeta)
+	setmetatable(ProductionState, ProductionStateMeta)
 
-   ProductionElement.State = ProductionState
+	ProductionElement.State = ProductionState
 
-   for Index, Function in ElementObject.Functions do
-      ProductionElement[Index] = Function
-   end
+	for Index, Function in ElementObject.Functions do
+		ProductionElement[Index] = Function
+	end
 
-   --
+	--
 
-   for Property: string, Value: any in ElementObject.Properties do
-      ElementInstance[Property] = Value
-   end
+	for Property: string, Value: any in ElementObject.Properties do
+		ElementInstance[Property] = Value
+	end
 
-   if ElementObject.Children then
-      for i, ChildElement in ElementObject.Children do
-         ChildElement.Properties.Parent = ElementInstance
-         ProductionChildren[i] = Essence.build(ChildElement)
-      end
-   end
+	if ElementObject.Children then
+		for i, ChildElement in ElementObject.Children do
+			ChildElement.Properties.Parent = ElementInstance
+			ProductionChildren[i] = Essence.build(ChildElement)
+		end
+	end
 
-   --
+	--
 
-   return ProductionElement
+	return ProductionElement
 end
 
 function Essence.getState(StateIdentifier: string, InitialValue: any, Compute: ((NewValue: any) -> any)?)
-   local StateObject = {}
+	local StateObject = {}
 
-   StateObject.StateIdentifier = StateIdentifier
-   StateObject.StateInitialValue = InitialValue
-   StateObject.StateCompute = Compute
-   StateObject.IsEssenceState = true
+	StateObject.StateIdentifier = StateIdentifier
+	StateObject.StateInitialValue = InitialValue
+	StateObject.StateCompute = Compute
+	StateObject.IsEssenceState = true
 
-   return StateObject
+	return StateObject
 end
 
 --
