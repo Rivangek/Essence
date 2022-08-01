7a2b63d5b362277823b7fde8e99753409f58048a
Rivangek <ank303442@gmail.com>
2022-08-01 10:55:53 -0500

    State managment & renderization

--- lib/Classes/Element.lua
+++ lib/Classes/Element.lua
@@ -1,10 +1,58 @@
 local Element = {}
 Element.__index = Element
 
+-- Typos
+
+export type Class = {
+   Children: { Class },
+   Properties: { [string]: any },
+   Functions: { [string]: (any) -> any },
+   ClassName: string,
+
+   SetState: (Class, StateIdentifier: string, NewValue: any) -> nil,
+   GetState: (Class, StateIdentifier: string) -> any
+}
+
 --
 
-function Element.new()
+function Element.new(Properties)
    local self = setmetatable({}, Element)
 
-   self._Children = {}
+   self.Children = Properties.Children
+   self.ClassName = Properties.ClassName
+   self.Functions = {}
+
+   Properties.Children = nil
+   Properties.ClassName = nil
+
+   self.Properties = Properties
+   self.OriginalProperties = table.clone(self.Properties)
+
+   self.State = {}
+
+   for Property: string, Value: any in self.Properties do
+      if typeof(Value) == "string" and string.match(Value, "STATE;") then -- Is State
+         local StateValues = string.split(Value, ";")
+
+         local StateIdentifier = StateValues[2]
+         local StateInitialValue = StateValues[3]
+
+         self.State[StateIdentifier] = StateInitialValue
+         self.Properties[Property] = self.State[StateIdentifier]
+      end
+   end
+
+   return self
 end
+
+function Element:SetState(StateId: string, StateValue: any)
+   self.State[StateId] = StateValue
+end
+
+function Element:GetState(StateId: string)
+   return self.State[StateId]
+end
+
+--
+
+return Element
--- lib/init.lua
+++ lib/init.lua
@@ -3,22 +3,102 @@ local Essence = {}
 local Classes = script:WaitForChild("Classes")
 local Modules = script:WaitForChild("Modules")
 
-local Component = require(Classes.Component)
+local Element = require(Classes.Element)
 
 --
 
-function Essence.new(Class: string, Properties: { any })
-   if Class == "Component" then
-      
+function Essence.new(...)
+   local Parameters = {...}
+
+   local ClassObject: string | { any } = Parameters[1] -- It can be an string containing the ClassName
+   local Props = Parameters[2]
+
+   if typeof(ClassObject) == "table" then -- It is a component.
+      local ElementObject = ClassObject:Render(Props)
+
+      for Index, Value in ClassObject do
+         if typeof(Value) == "function" and not (Index == "Render") then
+            ElementObject.Functions[Index] = Value
+         end
+      end
+
+      return ElementObject;
    end
 
    -- Switch not passed, it's a Roblox class.
 
-   
+   local ElementProperties = Props
+   ElementProperties.ClassName = ClassObject
+
+   local ElementObject = Element.new(ElementProperties)
+   return ElementObject
 end
 
-function Essence.mount()
+function Essence.build(ElementObject: Element.Class)
+   local ElementInstance = Instance.new(ElementObject.ClassName)
+   local OriginalProperties = ElementObject.OriginalProperties
 
+   --
+
+   local ProductionElement = {}
+   ProductionElement.Children = {}
+
+   local ProductionState = {}
+
+   --
+
+   local ProductionElementMeta = {
+      __index = function(tabl: { any }, index: string)
+         if index == "Instance" then
+            return ElementInstance
+         elseif index == "State" then
+            return ProductionState
+         end
+      end
+   }
+
+   local ProductionStateMeta = {
+      __newindex = function(_, index: string, value: any)
+         for Property: string, Value: any in OriginalProperties do
+            if typeof(Value) == "string" and string.match(Value, "STATE;") then
+               local StateValues = string.split(Value, ";")
+               local StateIdentifier = StateValues[2]
+
+               if index == StateIdentifier then
+                  ElementInstance[Property] = value
+                  ElementObject:SetState(StateIdentifier, value)
+               end
+            end
+         end
+      end,
+
+      __index = function(_, index: string)
+         return ElementObject:GetState(index)
+      end
+   }
+
+   setmetatable(ProductionElement, ProductionElementMeta)
+   setmetatable(ProductionState, ProductionStateMeta)
+
+   ProductionElement.State = ProductionState
+
+   for Index, Function in ElementObject.Functions do
+      ProductionElement[Index] = Function
+   end
+
+   --
+
+   for Property: string, Value: any in ElementObject.Properties do
+      ElementInstance[Property] = Value
+   end
+
+   --
+
+   return ProductionElement
+end
+
+function Essence.getState(StateIdentifier: string, InitialValue: any)
+   return "STATE;"..StateIdentifier..";"..InitialValue
 end
 
 --
